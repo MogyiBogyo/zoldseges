a0e855c1d2597199360f9851b88427e72847baa5
Vécsei Ágnes <vecsei.agnes96@gmail.com>
2020-02-21 12:22:29 +0100

    project initialize

---
+++ zoldseges/src/main/java/org/elte/zoldseges/ZoldsegesApplication.java
@@ -0,0 +1,13 @@
+package org.elte.zoldseges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoldsegesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoldsegesApplication.class, args);
+	}
+
+}
---
+++ zoldseges/src/test/java/org/elte/zoldseges/ZoldsegesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elte.zoldseges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoldsegesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
